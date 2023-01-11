d31879672dc94eb97911c622d0f17c979f40c788
Dhilip <dhilip.kumaran@kovai.co>
2023-01-11 23:10:19 +0530

    sorted sqi algo

---
+++ sorted-squared-array/Program.cs
@@ -0,0 +1,18 @@
+﻿using sorted_squared_array;
+using System.ComponentModel;
+using System;
+
+//Details:
+//Write a function that takes in a non-empty array of integers that are sorted
+//  in ascending order and returns a new array of the same length with the squares
+//  of the original integers also sorted in ascending order.
+//Test1
+var test1Input = new int[] { 1, 2, 3, 5, 6, 8, 9 };
+var test1Output = new int[] { 1, 4, 9, 25, 36, 64, 81 };
+var test1Result = Solutions.Solution1(test1Input);
+Console.WriteLine(Enumerable.SequenceEqual(test1Result, test1Output));
+
+var test2Input = new int[] { 1, 2, 3, 5, 6, 8, 9 };
+var test2Output = new int[] { 1, 4, 9, 25, 36, 64, 81 };
+var test2Result = Solutions.Solution2(test2Input);
+Console.WriteLine(Enumerable.SequenceEqual(test2Result, test2Output));
---
+++ sorted-squared-array/Solutions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorted_squared_array
+{
+    internal static class Solutions
+    {
+        public static int[] Solution1(int[] array)
+        {
+            // Write your code here.
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i] * array[i];
+            }
+            Array.Sort(array);
+            return array;
+        }
+            public static int[] Solution2(int[] array)
+            {
+                var res = new int[array.Length];
+                var counter = array.Length - 1;
+                var fi = 0;
+                var li = array.Length - 1;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (Math.Abs(array[fi]) > Math.Abs(array[li]))
+                    {
+                        res[counter-i] = array[fi] * array[fi];
+                        fi++;
+                    }
+                    else
+                    {
+                        res[counter-i] = array[li] * array[li];
+                        li--;
+                    }
+                }
+                return res;
+            }
+    }
+}
